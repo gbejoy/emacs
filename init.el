4c50ecee290a63559af205fc4d7025ba84e459e1
Gokul Bejoy <imgokulbejoy+code@gmail.com>
2024-12-05 18:59:27 +0530

    Added corfu, vertico, orderless and made configurations for all

--- init.el
+++ init.el
@@ -15,37 +15,73 @@
   (global-corfu-mode))
 
 ;;;; emacs
-
+;; Make changes to key parts of emacs itself
 (use-package emacs
   :config
   (set-frame-font "Source Code Pro-12" nil t)
   (load-theme 'leuven t)
   :custom
   (backup-directory-alist `(("." . ,(concat user-emacs-directory "backups"))))
+  ;; Support opening new minibuffers from inside existing minibuffers.
+  (enable-recursive-minibuffers t)
+  ;; Hide commands in M-x which do not work in the current mode.  Vertico
+  ;; commands are hidden in normal buffers. This setting is useful beyond
+  ;; Vertico.
+  (read-extended-command-predicate #'command-completion-default-include-p)
   (user-full-name "Gokul Bejoy")
-  (user-mail-address "imgokulbejoy@gmail.com"))
+  (user-mail-address "imgokulbejoy@gmail.com")
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+	'(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode))
 
 ;;;; cus-edit
-
+;; Change customize properties
 (use-package cus-edit
   :custom
   (custom-file "~/.emacs.d/custom.el"))
 
 ;;;; magit
+;; Git porcelain with good interface
 (use-package magit)
 
-;;;; org
+;;;; orderless
+;; Completion style that matches regex without respect to order
+(use-package orderless
+  :custom
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch))
+  ;; (orderless-component-separator #'orderless-escapable-split-on-space)
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
 
+;;;; org
+;; For notetaking, scheduling and many more
 (use-package org
   :custom
   (org-directory "~/Documents/org/")
   (org-default-notes-file (concat org-directory "inbox.org")))
 
 ;;;;; org-agenda
+;; Settings for agenda view
 (use-package org-agenda
   :bind ("C-c a" . org-agenda))
 
 ;;;;; org-capture
+;; Templates for jotting down different types of data
 (use-package org-capture
   :after org
   :bind ("C-c c" . org-capture)
@@ -60,6 +96,7 @@
      ("tt" "Todo Today" entry (file+headline "todo.org" "Misc") "* TODO %t\n %?\n %i\n"))))
 
 ;;;;; org-journal
+;; Write important events that happened in daily
 (use-package org-journal
   :bind ("C-c j" . org-journal-new-entry)
   :config
@@ -69,18 +106,42 @@
   (org-journal-dir (concat org-directory "journal/")))
 
 ;;;;; org-roam
+;; Zettelkaisen based note-taking solution in org-mode
 (use-package org-roam
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n i" . org-roam-node-insert))
   :ensure t
   :custom
   (org-roam-directory "~/Documents/vault/"))
 
 ;;;;; ox-hugo
+;; Export org-files into hugo pages
 (use-package ox-hugo
+  :ensure t
   :pin melpa
   :after ox)
 
 ;;;; pdf-tools
+;; Read pdf within emacs using poppler
 (use-package pdf-tools
   :magic ("%PDF" . pdf-view-mode)
   :config
   (pdf-tools-install :no-query))
+
+;;;; savehist
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+;;;; vertico
+;; VERTical COmpletion for better completion-at-point
+(use-package vertico
+;:custom
+;; (vertico-scroll-margin 0) ;; Different scroll margin
+;; (vertico-count 20) ;; Show more candidates
+;; (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
+;; (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
+:init
+(vertico-mode))
